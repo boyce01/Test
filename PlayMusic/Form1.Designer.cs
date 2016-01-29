8a0e5f5d14c1cfd231945e02f864b8bbcf418f01
boyce01 <boycezhao64@gmail.com>
2016-01-29 16:26:42 +1300

    PlayMusic

---
+++ PlayMusic/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace PlayMusic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnPre = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(70, 50);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(118, 34);
+            this.btnOpen.TabIndex = 0;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(70, 129);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(209, 304);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // btnPre
+            // 
+            this.btnPre.Location = new System.Drawing.Point(366, 129);
+            this.btnPre.Name = "btnPre";
+            this.btnPre.Size = new System.Drawing.Size(118, 34);
+            this.btnPre.TabIndex = 2;
+            this.btnPre.Text = "Previous";
+            this.btnPre.UseVisualStyleBackColor = true;
+            this.btnPre.Click += new System.EventHandler(this.btnPre_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(366, 288);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(118, 34);
+            this.btnNext.TabIndex = 3;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(772, 500);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPre);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnOpen);
+            this.Name = "Form1";
+            this.Text = "播放音乐";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnPre;
+        private System.Windows.Forms.Button btnNext;
+    }
+}
+
---
+++ PlayMusic/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Media;
+namespace PlayMusic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //声明一个List泛型集合,用于存放选中的音乐文件的全路径：
+        List<string> listSong = new List<string>();
+
+        /// <summary>
+        /// 选择要播放的音乐文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "请选择音乐文件";
+            ofd.Multiselect = true;
+            ofd.InitialDirectory = @"C:\Users\up\Desktop\music";
+            ofd.Filter = "音乐文件|*.wav|所有文件|*.*";
+            ofd.ShowDialog();
+            //获得所选音乐文件的全路径(可多选)
+            string[] path = ofd.FileNames;
+            //遍历所有选中的yinyuewenj,并在listBox1中显示：
+            for (int i = 0; i < path.Length; i++)
+            {
+                //Path.GetFileName()方法:用于获得文件名
+                listBox1.Items.Add(Path.GetFileName(path[i]));
+                //把选中音乐文件的全路径赋值给List泛型集合：
+                listSong.Add(path[i]);
+            }
+        }
+
+        //声明在方法外，以便于其他类调用
+        SoundPlayer sp = new SoundPlayer();
+
+        /// <summary>
+        /// 双击列表中的音乐,并播放该音乐
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            //将选中的文件的全路径(存在list集合中)赋值给SoundLocation
+            sp.SoundLocation = listSong[listBox1.SelectedIndex]; //listBox1.SelectedIndex:当前选中的文件的索引
+            sp.Play();
+        }
+
+        /// <summary>
+        /// 播放下一曲
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            //获得当前播放音乐的索引：
+            int index = listBox1.SelectedIndex;
+            index++; //播放下一曲：当前索引加1
+            if (index == listBox1.Items.Count)
+            {
+                index = 0;
+            }
+            //把新的索引赋值给当前选中项的索引，这样才能让蓝条变化
+            listBox1.SelectedIndex = index;
+            sp.SoundLocation = listSong[index];
+            sp.Play();
+        }
+
+        /// <summary>
+        /// 播放上一曲
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnPre_Click(object sender, EventArgs e)
+        {
+            //获得当前播放音乐文件的索引：
+            int index = listBox1.SelectedIndex;
+            index--;
+            if (index < 0)
+            {
+                index = listBox1.Items.Count-1;
+            }
+            listBox1.SelectedIndex = index;
+            sp.SoundLocation = listSong[index];
+            sp.Play();
+        }
+    }
+}
