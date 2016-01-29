18b7f84ee034487ff26803df0b9ec0987ef417a7
boyce01 <boycezhao64@gmail.com>
2016-01-29 16:35:35 +1300

    dylm

---
+++ DoYouLoveMe/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace DoYouLoveMe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        //当一个初始化一个窗体的时候，也会初始化窗体上的所有组件！！！
+        private void InitializeComponent() //开始初始化各个组件
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(75, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "love";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(291, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 39);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "unlove";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.MouseEnter += new System.EventHandler(this.button2_MouseEnter);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(102, 133);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(242, 83);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(128, 253);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 346);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DoYouLoveMe/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoYouLoveMe
+{
+    public partial class Form1 : Form
+    {
+        public Form1() //Form1类的构造函数
+        {
+            InitializeComponent(); //初始化组件的方法
+            //当一个初始化一个窗体的时候，也会初始化窗体上的所有组件！！！
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("yes"); //MessageBox.Show("");等于Console.WriteLine("");
+            this.Close(); //关闭Form1主窗体
+        }
+
+        /// <summary>
+        /// 当鼠标进入按钮可见部分时，给按钮一个新坐标
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_MouseEnter(object sender, EventArgs e)
+        {
+            //给按钮一个新坐标：
+            //这个按钮的最大宽度就是=窗体的宽度-按钮的宽度
+            //int x=this.Width; //this表示这个窗体；Width表示宽度（定值）
+            int x = this.ClientSize.Width - button2.Width; //ClientSize表示，当窗体大小变化时，size也会变化
+            int y = this.ClientSize.Height - button2.Height;
+            
+            //给unlove按钮一个随机的坐标：
+            Random r = new Random();
+            button2.Location = new Point(r.Next(0, x + 1), r.Next(0, y + 1));
+            //Location：该控件的位置坐标；
+            //new Point()：初始化一个点的坐标；
+            //r.Next(0,x):最大值取到x-1
+
+        }
+
+        /// <summary>
+        /// 当单击到button2时，展示信息，并关闭窗口
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("cateched");
+            this.Close();
+        }
+
+        /// <summary>
+        /// 当输入文本时，在label控件里显示所输入的文本
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text; //把TextBox文本框中的文本（值）赋值给label控件（显示出来）
+            //Text（是个属性）:输入的当前文本
+        }
+
+        
+    }
+}
